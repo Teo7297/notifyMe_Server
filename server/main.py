5a56661317833d4452e7711364c43bd244fd85b5
Matteo <teocava7297@gmail.com>
2020-09-03 15:02:05 +0200

    fix

--- server/main.py
+++ server/main.py
@@ -1,46 +1,48 @@
 import firebase_admin
 from firebase_admin import firestore
 import flask, json
-
+#import os
+#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/teo/Desktop/notifyMe_Server/NotifyMe-1acab18a931f.json"
 app = flask.Flask(__name__)
 
 firebase_admin.initialize_app()
-USERS = firestore.client().collection('users')
+DEVICES = firestore.client().collection('devices')
 
-@app.route('/users/register', methods=['POST'])
+@app.route('/new_device', methods=['POST'])
 def add_user():
     req = flask.request.json
-    user = USERS.document()
-    user.set(req)
-    return flask.jsonify({'id': user.id}), 201
+    device = DEVICES.document()
+    device.set(req)
+    return flask.jsonify({'token': device.token}), 201
 
-@app.route('/users/<id>', methods=['GET'])
-def read_user(id):
-    return flask.jsonify(_ensure_user(id).to_dict())
+@app.route('/devices/<token>', methods=['GET'])
+def read_devie(token):
+    return flask.jsonify(_ensure_device(token).to_dict())
 
-@app.route('/users', methods=['GET'])
-def read_users():
-    users = [u.to_dict() for u in USERS.get()]
-    return json.dumps(users)
+@app.route('/devices', methods=['GET'])
+def read_devices():
+    devices = [d.to_dict() for d in DEVICES.get()]
+    return json.dumps(devices)
 
-@app.route('/users/<id>', methods=['PUT'])
-def update_user(id):
-    _ensure_user(id)
+@app.route('/devices/<token>', methods=['PUT'])
+def update_device(token):
+    _ensure_device(token)
     req = flask.request.json
-    USERS.document(id).set(req)
+    DEVICES.document(token).set(req)
     return flask.jsonify({'success': True})
 
-@app.route('/users/<id>', methods=['DELETE'])
-def delete_user(id):
-    _ensure_user(id)
-    USERS.document(id).delete()
+@app.route('/devices/<token>', methods=['DELETE'])
+def delete_device(token):
+    _ensure_device(token)
+    DEVICES.document(token).delete()
     return flask.jsonify({'success': True})
 
-def _ensure_user(id):
+def _ensure_device(token):
     try:
-        return USERS.document(id).get()
+        return DEVICES.document(token).get()
     except:
         flask.abort(404)
 
 if __name__ == '__main__':
+    
     app.run(host='127.0.0.1', port=8080, debug=True)
